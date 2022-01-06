84897a558841c5f96c90ea76d265a4ebfde66691
sajib-hossan1 <sajib.hossan112@gmail.com>
2022-01-06 21:48:49 +0600

    first commit

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand brand-title" href="#">e-Commerce</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto me-4 navbar-links">
+                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" href="#">Products</a>
+                            <a className="nav-link" href="#">About Us</a>
+                        </div>
+                        <div>
+                            <button className="login-btn-header">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
